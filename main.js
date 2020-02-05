63fd7e38511e09898b3a3dc06bc20a36040344d4
Ally Jarjour <allyjarjour@gmail.com>
2020-02-04 23:49:29 -0700

    Highlight activity categories

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-var studyBox = document.querySelector('#study-cat');
-var idleStudy = document.querySelector('.idle-study');
+// change color of study category on click
 
-studyBox.addEventListener('click', changeStudyBorder);
+document.querySelector('#study-cat').addEventListener('click', changeStudyColor);
 
-function changeStudyBorder() {
-  idleStudy.innerHTML = src="assets/study-active.svg"
-  console.log('hello');
+function changeStudyColor() {
+  document.querySelector('#study-cat').style.border = "2px solid #B3FD78";
+  document.querySelector('#study-text').style.color = "#B3FD78";
+  document.querySelector('#idle-study').src = "assets/study-active.svg";
+}
+
+// change color of meditate category on click
+
+document.querySelector('#med-cat').addEventListener('click', changeMedColor);
+
+function changeMedColor() {
+  if (document.querySelector('#med-cat').style.border = "2px solid #FFF")
+  {
+    document.querySelector('#med-cat').style.border = "2px solid #C278FD";
+  } else {
+    document.querySelector('#med-cat').style.border = "2px solid #FFF";
+  }
+  if (document.querySelector('#idle-med').getAttribute('src') == "assets/meditate.svg")
+  {
+    document.querySelector('#idle-med').src = "assets/meditate-active.svg";
+  } else {document.querySelector('#idle-med').src = "assets/meditate.svg";
+  }
+  document.querySelector('#med-text').style.color = "#C278FD";
+}
+
+// change color of meditate category on click
+
+document.querySelector('#exercise-cat').addEventListener('click', changeExerciseIcon);
+
+function changeExerciseIcon() {
+    document.querySelector('#idle-exercise').src = "assets/exercise-active.svg";
+    document.querySelector('#exercise-text').style.color = "#FD8078";
+    document.querySelector('#exercise-cat').style.border = "2px solid #FD8078";
 }
