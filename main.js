262c8faf8da43f64b03a72d341674f6125be6ae0
Ally Jarjour <allyjarjour@gmail.com>
2020-02-04 17:49:39 -0700

    Add event listener to study category

---
+++ main.js
@@ -0,0 +1,9 @@
+var studyBox = document.querySelector('#study-cat');
+var idleStudy = document.querySelector('.idle-study');
+
+studyBox.addEventListener('click', changeStudyBorder);
+
+function changeStudyBorder() {
+  idleStudy.innerHTML = src="assets/study-active.svg"
+  console.log('hello');
+}
