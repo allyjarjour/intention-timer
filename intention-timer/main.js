9ff1e0ecf871313f323762ee805e5d7be1c236e2
Katy St Sauveur <katyrogowski@yahoo.com>
2020-02-07 11:56:37 -0700

    Removed extra project

--- intention-timer/main.js
+++
@@ -1,9 +0,0 @@
-var studyBox = document.querySelector('#study-cat');
-var idleStudy = document.querySelector('.idle-study');
-
-studyBox.addEventListener('click', changeStudyBorder);
-
-function changeStudyBorder() {
-  idleStudy.innerHTML = src="assets/study-active.svg"
-  console.log('hello');
-}
