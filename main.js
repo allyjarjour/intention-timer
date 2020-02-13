2188a6a29a22795e138f63a219874d4ea7438636
Katy St Sauveur <katyrogowski@yahoo.com>
2020-02-12 22:05:44 -0700

    Final JS format

--- main.js
+++ main.js
@@ -1,28 +1,72 @@
 // study category variables
-
 var studyBox = document.querySelector('#study-cat');
 var studyActive = document.querySelector('#study-cat-active');
-
-
-// meditation category variables
-
 var medBox = document.querySelector('#med-cat');
 var medActive = document.querySelector('#med-cat-active');
-
-
-// exercise category variables
-
 var exBox = document.querySelector('#exercise-cat');
 var exActive = document.querySelector('#exercise-cat-active');
 
-// variables, function and event listener for switching to timer card
-
+// variables for switching to timer card
 var startButton = document.querySelector('.start-button');
 var mainCard = document.querySelector('.left-side');
 var timerCard = document.querySelector('.timer-card');
 
+var accompAlert = document.querySelector('#accomp-alert');
+var minuteAlert = document.querySelector('#minute-alert');
+var secondAlert = document.querySelector('#second-alert');
+var currentActTitle = document.querySelector('.current-activity-title');
+var catAlert = document.querySelector('#category-alert');
+var logActButton = document.querySelector('.log-act-button');
+
+var minuteInput = document.querySelector('#minute-input');
+var secondInput = document.querySelector('#second-input');
+
+var accompInput = document.querySelector('.accomp-input');
+
+var medClickCount = 0;
+var exClickCount = 0;
+var studyClickCount = 0;
+
+var card2Text = document.querySelector('.card2-text');
+
+var minuteInput = document.querySelector('#minute-input');
+var secondInput = document.querySelector('#second-input');
+var seconds = document.querySelector('.seconds');
+var minutes = document.querySelector('.minutes');
+
+var timerButton = document.querySelector('.timer-button');
+
+var allCategoryBoxes = document.querySelectorAll('#category-box-container');
+var cardContainer = document.querySelector('.card-container');
+var logMessage = document.querySelector('.log-message');
+var timer = document.querySelector('.timer');
+var activitySelector;
 
 startButton.addEventListener('click', addTimer)
+
+studyBox.addEventListener('click', switchToStudyActive);
+medBox.addEventListener('click', switchToMedActive);
+exBox.addEventListener('click', switchToExActive);
+
+startButton.addEventListener('click', addAccompAlert);
+startButton.addEventListener('click', addMinuteAlert);
+startButton.addEventListener('click', addSecondAlert);
+startButton.addEventListener('click', addActivityAlert);
+
+exBox.addEventListener('click', changeTimerEx);
+medBox.addEventListener('click', changeTimerMed);
+studyBox.addEventListener('click', changeTimerStudy);
+
+startButton.addEventListener('click', changeCard2Text);
+
+startButton.addEventListener('click', showTime);
+
+startButton.addEventListener('click', addTotalSeconds);
+
+timerButton.addEventListener('click', timerStart);
+
+logActButton.addEventListener('click', addPastAct);
+
 
 function addTimer() {
   if (accompInput.value.length > 0 && minuteInput.value.length > 0 && secondInput.value.length > 0) {
@@ -31,15 +75,6 @@ function addTimer() {
     currentActTitle.classList.remove('hidden');
   }
 }
-
-// (studyBox.onclick == true || medBox.onclick == true || exBox.onclick == true)
-
-var accompAlert = document.querySelector('#accomp-alert');
-var minuteAlert = document.querySelector('#minute-alert');
-var secondAlert = document.querySelector('#second-alert');
-var currentActTitle = document.querySelector('.current-activity-title');
-var catAlert = document.querySelector('#category-alert');
-var logActButton = document.querySelector('.log-act-button');
 
 window.onload = function hideTimerOnload() {
   timerCard.classList.add('hidden');
@@ -54,10 +89,6 @@ window.onload = function hideTimerOnload() {
   logActButton.classList.add('hidden');
 }
 
-// toggle categories - meditate, study and exercise
-
-studyBox.addEventListener('click', switchToStudyActive);
-
 function switchToStudyActive() {
   studyBox.classList.add('hidden');
   studyActive.classList.remove('hidden');
@@ -68,8 +99,6 @@ function switchToStudyActive() {
   activitySelector = "Study";
   borderSelector = '#B3FD78';
 }
-
-medBox.addEventListener('click', switchToMedActive);
 
 function switchToMedActive() {
   studyActive.classList.add('hidden');
@@ -82,8 +111,6 @@ function switchToMedActive() {
   borderSelector = '#C278FD';
 }
 
-exBox.addEventListener('click', switchToExActive);
-
 function switchToExActive() {
   studyActive.classList.add('hidden');
   exBox.classList.add('hidden');
@@ -95,20 +122,10 @@ function switchToExActive() {
   borderSelector = '#FD8078';
 }
 
-// input validation
-
-var minuteInput = document.querySelector('#minute-input');
-var secondInput = document.querySelector('#second-input');
-
-
 function numbersOnly(minuteInput) {
   var regEx = /[^0-9]/g;
   minuteInput.value = minuteInput.value.replace(regEx, " ");
 }
-// add activity alert
-var accompInput = document.querySelector('.accomp-input');
-
-startButton.addEventListener('click', addAccompAlert);
 
 function addAccompAlert() {
   if (accompInput.value.length === 0) {
@@ -116,32 +133,17 @@ function addAccompAlert() {
   }
 }
 
-// add minute alert
-
-startButton.addEventListener('click', addMinuteAlert);
-
 function addMinuteAlert() {
   if (minuteInput.value.length === 0) {
     minuteAlert.classList.remove('hidden');
   }
 }
 
-// add second alert
-
-startButton.addEventListener('click', addSecondAlert);
-
 function addSecondAlert() {
   if (secondInput.value.length === 0) {
     secondAlert.classList.remove('hidden');
   }
 }
-
-// add category alert
-
-var medClickCount = 0;
-var exClickCount = 0;
-var studyClickCount = 0;
-
 
 medBox.onclick = function() {
   var medClickCount = 1;
@@ -158,21 +160,11 @@ studyBox.onclick = function() {
   return studyBox;
 }
 
-startButton.addEventListener('click', addActivityAlert);
-
 function addActivityAlert() {
   if (medClickCount === 0 || exClickCount === 0 || studyClickCount === 0) {
     catAlert.classList.remove('hidden');
+  }
 }
-}
-
-
-
-// change timer button
-
-exBox.addEventListener('click', changeTimerEx);
-medBox.addEventListener('click', changeTimerMed);
-studyBox.addEventListener('click', changeTimerStudy);
 
 function changeTimerEx() {
   timerButton.style.border = "1px solid #FD8078";
@@ -186,39 +178,18 @@ function changeTimerStudy() {
   timerButton.style.border = "1px solid #B3FD78";
 }
 
-// change card two text for what you want to accomp
-
-var card2Text = document.querySelector('.card2-text');
-
-startButton.addEventListener('click', changeCard2Text);
-
 function changeCard2Text() {
   card2Text.innerText = accompInput.value;
 }
-
-// -----timer
-
-var minuteInput = document.querySelector('#minute-input');
-var secondInput = document.querySelector('#second-input');
-var seconds = document.querySelector('.seconds');
-var minutes = document.querySelector('.minutes');
-
-startButton.addEventListener('click', showTime);
 
 function showTime(){
   minutes.innerText = minuteInput.value;
   seconds.innerText = secondInput.value;
 }
 
-startButton.addEventListener('click', addTotalSeconds);
-
 function addTotalSeconds() {
   return ((parseInt(minuteInput.value) * 60) + (parseInt(secondInput.value)));
 }
-
-var timerButton = document.querySelector('.timer-button');
-
-timerButton.addEventListener('click', timerStart);
 
 function timerStart() {
   var myTimer = setInterval(clock, 1000);
@@ -236,30 +207,18 @@ function timerStart() {
       clearInterval(myTimer);
       timerButton.innerText = "YOU'RE AMAZING!";
       logActButton.classList.remove('hidden');
+    }
   }
-  }
-  }
+}
 
-  //Add past activity cards.
-
-  var allCategoryBoxes = document.querySelectorAll('#category-box-container');
-  var cardContainer = document.querySelector('.card-container');
-  var logMessage = document.querySelector('.log-message');
-  var timer = document.querySelector('.timer');
-  var activitySelector;
-  // var borderSelector;
-
-logActButton.addEventListener('click', addPastAct);
-
-  function addPastAct() {
-    cardContainer.insertAdjacentHTML('afterbegin', `
-      <p class="past-act-card"><span class="act-selector">${activitySelector}</span><br />
-      <span>${minuteInput.value} MINUTES </span>
-      <span>${secondInput.value} SECONDS</span><br /><span>${accompInput.value}</span></p>`);
-    logMessage.classList.add('hidden');
-    currentActTitle.innerText = ('Completed Activity');
-    card2Text.classList.add('hidden');
-    timer.classList.add('hidden');
-    timerButton.classList.add('hidden');
-    // document.querySelector('.past-act-card').style.border('1px solid ${borderSelector}');
-  }
+function addPastAct() {
+  cardContainer.insertAdjacentHTML('afterbegin', `
+    <p class="past-act-card"><span class="act-selector">${activitySelector}</span><br />
+    <span>${minuteInput.value} MINUTES </span>
+    <span>${secondInput.value} SECONDS</span><br /><span>${accompInput.value}</span></p>`);
+  logMessage.classList.add('hidden');
+  currentActTitle.innerText = ('Completed Activity');
+  card2Text.classList.add('hidden');
+  timer.classList.add('hidden');
+  timerButton.classList.add('hidden');
+}
