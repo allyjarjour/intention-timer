41be43b0a12b66ac42dfa9e6d47637953e27cc92
Ally Jarjour <allyjarjour@gmail.com>
2020-03-13 11:09:32 -0400

    Add switch statement to category toggling

--- main.js
+++ main.js
@@ -1,72 +1,50 @@
-// study category variables
 var studyBox = document.querySelector('#study-cat');
 var studyActive = document.querySelector('#study-cat-active');
 var medBox = document.querySelector('#med-cat');
 var medActive = document.querySelector('#med-cat-active');
 var exBox = document.querySelector('#exercise-cat');
 var exActive = document.querySelector('#exercise-cat-active');
-
-// variables for switching to timer card
 var startButton = document.querySelector('.start-button');
 var mainCard = document.querySelector('.left-side');
 var timerCard = document.querySelector('.timer-card');
-
 var accompAlert = document.querySelector('#accomp-alert');
 var minuteAlert = document.querySelector('#minute-alert');
 var secondAlert = document.querySelector('#second-alert');
 var currentActTitle = document.querySelector('.current-activity-title');
 var catAlert = document.querySelector('#category-alert');
 var logActButton = document.querySelector('.log-act-button');
-
 var minuteInput = document.querySelector('#minute-input');
 var secondInput = document.querySelector('#second-input');
-
 var accompInput = document.querySelector('.accomp-input');
-
 var medClickCount = 0;
 var exClickCount = 0;
 var studyClickCount = 0;
-
 var card2Text = document.querySelector('.card2-text');
-
 var minuteInput = document.querySelector('#minute-input');
 var secondInput = document.querySelector('#second-input');
 var seconds = document.querySelector('.seconds');
 var minutes = document.querySelector('.minutes');
-
 var timerButton = document.querySelector('.timer-button');
-
-var allCategoryBoxes = document.querySelectorAll('#category-box-container');
+var allCategoryBoxes = document.querySelector('#category-box-container');
 var cardContainer = document.querySelector('.card-container');
 var logMessage = document.querySelector('.log-message');
 var timer = document.querySelector('.timer');
 var activitySelector;
 
 startButton.addEventListener('click', addTimer)
-
-studyBox.addEventListener('click', switchToStudyActive);
-medBox.addEventListener('click', switchToMedActive);
-exBox.addEventListener('click', switchToExActive);
-
 startButton.addEventListener('click', addAccompAlert);
 startButton.addEventListener('click', addMinuteAlert);
 startButton.addEventListener('click', addSecondAlert);
 startButton.addEventListener('click', addActivityAlert);
-
 exBox.addEventListener('click', changeTimerEx);
 medBox.addEventListener('click', changeTimerMed);
 studyBox.addEventListener('click', changeTimerStudy);
-
 startButton.addEventListener('click', changeCard2Text);
-
 startButton.addEventListener('click', showTime);
-
 startButton.addEventListener('click', addTotalSeconds);
-
 timerButton.addEventListener('click', timerStart);
-
 logActButton.addEventListener('click', addPastAct);
-
+allCategoryBoxes.addEventListener('click', addActiveStates);
 
 function addTimer() {
   if (accompInput.value.length > 0 && minuteInput.value.length > 0 && secondInput.value.length > 0) {
@@ -87,6 +65,20 @@ window.onload = function hideTimerOnload() {
   currentActTitle.classList.add('hidden');
   catAlert.classList.add('hidden');
   logActButton.classList.add('hidden');
+}
+
+function addActiveStates(event) {
+  switch(event.target.id) {
+    case 'med-cat':
+      switchToMedActive();
+      break;
+    case 'exercise-cat':
+      switchToExActive();
+      break;
+    case 'study-cat':
+      switchToStudyActive();
+      break;
+  }
 }
 
 function switchToStudyActive() {
